73562fecfec8603a349893837a9c7e3eecac6d95
Peter Dennis Bartok <pdb0102@users.noreply.github.com>
2024-12-27 23:19:48 -0700

    - no longer needed

--- mldsa/RandomBytes.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Security.Cryptography;
-
-namespace mldsa_net;
-public abstract partial class DilithiumBase {
-	private void randombytes(out byte[] out_buffer, int outlen) {
-		out_buffer = RandomNumberGenerator.GetBytes(outlen);
-	}
-}
